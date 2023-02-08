848d14ba677cb49b4d1b6ba34fa204982c38d0d1
zhongming24 <18612897485@163.com>
2023-02-08 16:30:29 +0800

    独孤九剑

---
+++ src/main/java/com/atguigu/wugong/Dugujiujian.java
@@ -0,0 +1,10 @@
+package com.atguigu.wugong;
+
+public class Dugujiujian {
+    public static void main(String[] args) {
+        System.out.println("独");
+        System.out.println("孤");
+        System.out.println("九");
+        System.out.println("剑");
+    }
+}
